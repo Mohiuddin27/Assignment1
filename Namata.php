460a1aa25ed8d32c268490793f8c9ba58a2dd8d3
mohiuddin <sobujmohiuddin@gmail.com>
2021-08-05 14:23:26 +0600

    modified

--- Namata.php
+++ Namata.php
@@ -1,41 +1,12 @@
 <?php  
-for($i=1;$i<=10;$i++){
-    $num=2;
-    echo "{$num} x {$i}=".$num * $i."<br>";
-}
-echo "<br>";
-for($i=1;$i<=10;$i++){
-    $num=3;
-    echo "{$num} x {$i}=".$num * $i."<br>";
-}
-echo "<br>";
-for($i=1;$i<=10;$i++){
-    $num=4;
-    echo "{$num} x {$i}=".$num * $i."<br>";
-}
-echo "<br>";
-for($i=1;$i<=10;$i++){
-    $num=5;
-    echo "{$num} x {$i}=".$num * $i."<br>";
-}
-echo "<br>";
-for($i=1;$i<=10;$i++){
-    $num=6;
-    echo "{$num} x {$i}=".$num * $i."<br>";
-}
-echo "<br>";
-for($i=1;$i<=10;$i++){
-    $num=7;
-    echo "{$num} x {$i}=".$num * $i."<br>";
-}
-echo "<br>";
-for($i=1;$i<=10;$i++){
-    $num=8;
-    echo "{$num} x {$i}=".$num * $i."<br>";
-}
-echo "<br>";
-for($i=1;$i<=10;$i++){
-    $num=9;
-    echo "{$num} x {$i}=".$num * $i."<br>";
+for($i=2;$i<=9;$i++){
+    if($i<=10){
+        echo " multiplication number={$i}<br>";
+         
+    }
+    for($m=1;$m<=10;$m++){
+        echo "{$i} x {$m}= ".($i *$m)."<br>";
+    }
+
 }
 ?>
